6f9d515fe1dff8112a097999a5631f9990fbf317
Andrey Korchagin <korchagin.andrey.i@ya.ru>
2020-08-10 10:00:53 +0300

    Task7

--- Task7/src/main/java/com/korchagin/java/courses/task7/controllers/UsersController.java
+++ Task7/src/main/java/com/korchagin/java/courses/task7/controllers/UsersController.java
@@ -3,13 +3,11 @@ package com.korchagin.java.courses.task7.controllers;
 import com.korchagin.java.courses.task7.entity.Users;
 import com.korchagin.java.courses.task7.services.UsersService;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
 import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -24,17 +22,37 @@ public class UsersController {
         return "root-page";
     }
 
-    @GetMapping("/users")
-    public String showAllUsers(Model model){
-        model.addAttribute("users", usersService.getAllUsers());
+    @GetMapping("/users/all")
+    public String showAllUsers(Model model, @RequestParam(required = false) Integer maxAge){
+
+        model.addAttribute("users", usersService.getUsersWithFilter(maxAge));
         return "users-page";
     }
 
     @PostMapping("/users/add")
-    public String addUsers(@ModelAttribute Users user){
-//        model.addAttribute()
-        System.out.println(user.getUserName() + " " + user.getAge());
+    public String addUser(@ModelAttribute Users user){
         usersService.saveOrUpdateUser(user);
-        return "redirect:/users";
+        return "redirect:/users/all";
     }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditUserForm(@PathVariable Long id, Model model){
+        model.addAttribute("users", usersService.getUserById(id));
+//        usersService.saveOrUpdateUser(user);
+        return "users-edit-page";
+    }
+
+    @PostMapping("/users/edit")
+    public String editUser(@ModelAttribute Users user){
+        usersService.saveOrUpdateUser(user);
+        return "redirect:/users/all";
+    }
+
+    @GetMapping("/users/remove/{id}")
+    public String removeUserById(@PathVariable Long id){
+        usersService.deleteUserById(id);
+        return "redirect:/users/all";
+    }
+
+
 }
--- Task7/src/main/java/com/korchagin/java/courses/task7/services/UsersService.java
+++ Task7/src/main/java/com/korchagin/java/courses/task7/services/UsersService.java
@@ -3,6 +3,7 @@ package com.korchagin.java.courses.task7.services;
 import com.korchagin.java.courses.task7.entity.Users;
 import com.korchagin.java.courses.task7.repositories.UsersRepository;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +13,22 @@ import java.util.List;
 public class UsersService {
     private UsersRepository usersRepository;
 
-    public List<Users> getAllUsers(){
-        return usersRepository.findAll();
+    public List<Users> getUsers(){ return usersRepository.findAll(); }
+
+    public List<Users> getUsersWithFilter(Integer maxAge){
+        List<Users> users = usersRepository.findAll();
+        if (maxAge == null){
+            return users;
+        }
+        users.removeIf(u -> u.getAge() > maxAge);
+        return users;
     }
 
     public Users getUserById(Long id){
-        return usersRepository.getOne(id);
+        return usersRepository.findById(id).orElseThrow();
     }
 
-    public Users saveOrUpdateUser(Users user){
-        return usersRepository.save(user);
-    }
+    public Users saveOrUpdateUser(Users user){ return usersRepository.save(user); }
 
     public void saveAllUsers(List<Users> users){
         usersRepository.saveAll(users);
