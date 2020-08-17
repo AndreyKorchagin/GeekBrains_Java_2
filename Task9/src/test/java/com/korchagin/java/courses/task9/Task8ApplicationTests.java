cdef32ab9662208d2aabe37943451e833198b259
Andrey Korchagin <korchagin.andrey.i@ya.ru>
2020-08-17 12:24:35 +0300

    Task9

---
+++ Task9/src/test/java/com/korchagin/java/courses/task9/Task8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korchagin.java.courses.task9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
