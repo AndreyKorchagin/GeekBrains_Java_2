d51ab360d20349bc637864230e8579aef32afe7e
Andrey Korchagin <korchagin.andrey.i@ya.ru>
2020-08-28 00:38:26 +0300

    Task12

---
+++ Task12/src/main/java/com/geekbrains/book/store/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://vtb.com/geekbrains/book/store", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.geekbrains.book.store;
